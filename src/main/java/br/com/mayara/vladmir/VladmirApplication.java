6714e9636dce87df707944ff2f7e8411833c01d8
Mayara Silva <ssmayarah@gmail.com>
2020-11-16 17:38:23 -0300

    Primeiro commit

---
+++ src/main/java/br/com/mayara/vladmir/VladmirApplication.java
@@ -0,0 +1,13 @@
+package br.com.mayara.vladmir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VladmirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VladmirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mayara/vladmir/VladmirApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayara.vladmir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VladmirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
